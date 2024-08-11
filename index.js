e7209569d35eb7c478e1e63565ef213001472923
Himal Sandaruwan <wahsandaruwan6@gmail.com>
2024-08-11 14:28:19 +0530

    Updated the code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Initialize the Model, View, and Controller
   const model = new Model();
   const view = new View();
   const controller = new Controller(model, view);
